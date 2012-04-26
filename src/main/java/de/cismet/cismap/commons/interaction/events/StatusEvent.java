6b43e58b6d0729150ca2a3abb6a740f23b7523c8
Thorsten Herter <thorsten.herter@cismet.de>
2012-04-26 17:07:21 +0000

    [issue:0002545] Fix some problems with the legend after a restart

--- src/main/java/de/cismet/cismap/commons/interaction/events/StatusEvent.java
+++ src/main/java/de/cismet/cismap/commons/interaction/events/StatusEvent.java
@@ -33,6 +33,7 @@ public class StatusEvent {
     public static final String RETRIEVAL_RESET = "retrieval.reset";
     public static final String MAP_EXTEND_FIXED = "map.extent.fixed";
     public static final String MAP_SCALE_FIXED = "map.scale.fixed";
+    public static final String AWAKED_FROM_DUMMY = "awaked.from.dummy";
 
     //~ Instance fields --------------------------------------------------------
 
