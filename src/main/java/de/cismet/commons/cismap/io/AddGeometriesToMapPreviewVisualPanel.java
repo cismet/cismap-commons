af641f17d4ba06bf32863ebbd7b73b3000c822fc
Martin Scholl <martin.scholl@cismet.de>
2013-04-19 15:57:28 +0200

    [issue #40] added map initialisation code with hardcoded parameters so that wuppertal may receive a preview release

--- src/main/java/de/cismet/commons/cismap/io/AddGeometriesToMapPreviewVisualPanel.java
+++ src/main/java/de/cismet/commons/cismap/io/AddGeometriesToMapPreviewVisualPanel.java
@@ -14,6 +14,15 @@ import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import de.cismet.cismap.commons.Crs;
+import de.cismet.cismap.commons.XBoundingBox;
+import de.cismet.cismap.commons.features.DefaultStyledFeature;
+import de.cismet.cismap.commons.features.StyledFeature;
+import de.cismet.cismap.commons.gui.MappingComponent;
+import de.cismet.cismap.commons.gui.layerwidget.ActiveLayerModel;
+import de.cismet.cismap.commons.raster.wms.simple.SimpleWMS;
+import de.cismet.cismap.commons.raster.wms.simple.SimpleWmsGetMapUrl;
+
 /**
  * DOCUMENT ME!
  *
@@ -69,6 +78,33 @@ public class AddGeometriesToMapPreviewVisualPanel extends JPanel {
      * DOCUMENT ME!
      */
     private void initMap() {
+        final String simpleWms =
+            "http://S102X284:8399/arcgis/services/ALKIS-EXPRESS/MapServer/WMSServer?&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=FALSE&BGCOLOR=0xF0F0F0&EXCEPTIONS=application/vnd.ogc.se_xml&LAYERS=2,4,5,6,7,8,10,11,12,13,14,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31,32,33,34,35,36,37,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100&STYLES=&BBOX=<cismap:boundingBox>&WIDTH=<cismap:width>&HEIGHT=<cismap:height>&SRS=<cismap:srs>";
+        final XBoundingBox box = new XBoundingBox(model.getGeometry().getEnvelope().buffer(5.0));
+        final ActiveLayerModel mappingModel = new ActiveLayerModel();
+        mappingModel.setSrs(new Crs("EPSG:25832", "EPSG:25832", "EPSG:25832", true, true));
+        mappingModel.addHome(box);
+
+        final SimpleWMS swms = new SimpleWMS(new SimpleWmsGetMapUrl(simpleWms));
+        swms.setName("swms");
+        final StyledFeature dsf = new DefaultStyledFeature();
+        dsf.setGeometry(model.getGeometry());
+        // add the raster layer to the model
+        mappingModel.addLayer(swms);
+        // set the model
+        previewMap.setMappingModel(mappingModel);
+        // initial positioning of the map
+        final int duration = previewMap.getAnimationDuration();
+        previewMap.setAnimationDuration(0);
+        previewMap.gotoInitialBoundingBox();
+        // interaction mode
+        previewMap.setInteractionMode(MappingComponent.ZOOM);
+        // finally when all configurations are done ...
+        previewMap.unlock();
+        previewMap.setInteractionMode("MUTE");
+        previewMap.getFeatureCollection().addFeature(dsf);
+        previewMap.setAnimationDuration(duration);
+        previewMap.zoomToFeatureCollection();
     }
 
     /**
