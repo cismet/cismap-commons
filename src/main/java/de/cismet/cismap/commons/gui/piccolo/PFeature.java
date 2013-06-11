0cda99f43230b3df8b75cf7ab409dd8c439a1678
therter <thorsten.herter@cismet.de>
2013-06-11 15:45:34 +0200

    jalopy minor changes

--- src/main/java/de/cismet/cismap/commons/gui/piccolo/PFeature.java
+++ src/main/java/de/cismet/cismap/commons/gui/piccolo/PFeature.java
@@ -2203,8 +2203,10 @@ public class PFeature extends PPath implements Highlightable, Selectable, Refres
                         try {
                             pi.removeChild(infoNode);
                         } catch (NullPointerException e) {
-                            // This happens when you move a LinearReferencedLineFeature on the map
-                            log.debug("removeChild throws a NullPointerException", e);
+                            if (log.isDebugEnabled()) {
+                                // This happens when you move a LinearReferencedLineFeature on the map
+                                log.debug("removeChild throws a NullPointerException", e);
+                            }
                         }
                         piSelected.addChild(infoNode);
                     }
