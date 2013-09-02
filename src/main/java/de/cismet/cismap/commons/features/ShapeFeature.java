be44fec8cf3fca55b2a93ab22be5458a13202a31
Matthias Roncoroni <matthias.roncoroni@cismet.de>
2013-09-02 15:33:29 +0200

    #67 fixed: ShapeFeature used old Styling

--- src/main/java/de/cismet/cismap/commons/features/ShapeFeature.java
+++ src/main/java/de/cismet/cismap/commons/features/ShapeFeature.java
@@ -16,14 +16,16 @@ import com.vividsolutions.jts.geom.Geometry;
 import org.deegree.model.feature.FeatureProperty;
 import org.deegree.model.spatialschema.JTSAdapter;
 
+import org.deegree.feature.types.FeatureType;
+
+import org.deegree.style.se.unevaluated.Style;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import org.deegree.feature.types.FeatureType;
 
 import javax.xml.namespace.QName;
-import org.deegree.style.se.unevaluated.Style;
 
 /**
  * Features read from a SHP File.
