b9fad279a0cd2a4d41d7bb720c1d82bdb6a869a5
Matthias Roncoroni <matthias.roncoroni@cismet.de>
2013-09-02 09:12:12 +0200

    #67 changed stickyPNodes from List<PNode> to List<PSticky>

--- src/main/java/de/cismet/cismap/commons/gui/MappingComponent.java
+++ src/main/java/de/cismet/cismap/commons/gui/MappingComponent.java
@@ -936,7 +936,7 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
                 if ((each != null) && each.getVisible()) {
                     stickyNodeList.add(each);
                 } else {
-                    if ((each instanceof PSticky) && (each.getParent() == null)) {
+                    if ((each.getParent() == null)) {
                         removeStickyNode(each);
                     }
                 }
