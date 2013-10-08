22bb019e9fe7e2b3663a31983cca7fede25f9bec
Gilles Baatz <gilles.baatz@cismet.de>
2013-10-08 14:59:20 +0200

    #78 MappingComponent: the use of the list repaintListeners is synchronized

--- src/main/java/de/cismet/cismap/commons/gui/MappingComponent.java
+++ src/main/java/de/cismet/cismap/commons/gui/MappingComponent.java
@@ -266,7 +266,7 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
     private final HashMap<String, PLayer> featureGrpLayerMap = new HashMap<String, PLayer>();
     private BoundingBox initialBoundingBox;
 
-    private ArrayList<RepaintListener> repaintListeners = new ArrayList<RepaintListener>();
+    private final ArrayList<RepaintListener> repaintListeners = new ArrayList<RepaintListener>();
 
     //~ Constructors -----------------------------------------------------------
 
@@ -5044,7 +5044,10 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
      * @param  repaintListener  DOCUMENT ME!
      */
     public void addRepaintListener(final RepaintListener repaintListener) {
-        repaintListeners.add(repaintListener);
+        synchronized (repaintListeners) {
+            repaintListeners.add(repaintListener);
+            LOG.error(repaintListeners.size());
+        }
     }
 
     /**
@@ -5053,7 +5056,9 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
      * @param  repaintListener  DOCUMENT ME!
      */
     public void removeRepaintListener(final RepaintListener repaintListener) {
-        repaintListeners.remove(repaintListener);
+        synchronized (repaintListeners) {
+            repaintListeners.remove(repaintListener);
+        }
     }
 
     /**
@@ -5062,8 +5067,10 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
      * @param  e  DOCUMENT ME!
      */
     public void fireRepaintComplete(final RepaintEvent e) {
-        for (final RepaintListener repaintListener : repaintListeners) {
-            repaintListener.repaintComplete(e);
+        synchronized (repaintListeners) {
+            for (final RepaintListener repaintListener : (ArrayList<RepaintListener>)repaintListeners.clone()) {
+                repaintListener.repaintComplete(e);
+            }
         }
     }
 
@@ -5073,8 +5080,10 @@ public final class MappingComponent extends PSwingCanvas implements MappingModel
      * @param  e  DOCUMENT ME!
      */
     public void fireRepaintError(final RepaintEvent e) {
-        for (final RepaintListener repaintListener : repaintListeners) {
-            repaintListener.repaintError(e);
+        synchronized (repaintListeners) {
+            for (final RepaintListener repaintListener : (ArrayList<RepaintListener>)repaintListeners.clone()) {
+                repaintListener.repaintError(e);
+            }
         }
     }
 
