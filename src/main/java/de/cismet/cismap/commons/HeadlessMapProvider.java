6dc7f1862d80adef0782db9e28f54f9c0a0e68b1
Gilles Baatz <gilles.baatz@cismet.de>
2013-10-14 17:05:59 +0200

    Revert "Revert "Merge branch 'bug/85-pritingbug-when-not-in-default-crs' into dev""
    This reverts commit 37cb89bb8bf1322b5a0375037b450ce4d24581a9.

--- src/main/java/de/cismet/cismap/commons/HeadlessMapProvider.java
+++ src/main/java/de/cismet/cismap/commons/HeadlessMapProvider.java
@@ -223,7 +223,18 @@ public class HeadlessMapProvider {
             }
         }
 
+        ((ActiveLayerModel)headlessMapProvider.getMappingComponent().getMappingModel()).setSrs(
+            mappingComponent.getMappingModel().getSrs());
         return headlessMapProvider;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public MappingComponent getMappingComponent() {
+        return map;
     }
 
     /**
@@ -513,6 +524,7 @@ public class HeadlessMapProvider {
     public void setCenterMapOnResize(final boolean b) {
         centerMapOnResize = b;
     }
+
     /**
      * DOCUMENT ME!
      *
