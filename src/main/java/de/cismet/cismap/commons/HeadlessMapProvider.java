e971f5b21cbabc9e14edffed5cbb7bfcd67077bb
therter <thorsten.herter@cismet.de>
2013-11-19 16:49:55 +0100

    #87 initialises the MappingComponent of the HeadlessMapProvider with all home bounding boxes from the master MappingComponent

--- src/main/java/de/cismet/cismap/commons/HeadlessMapProvider.java
+++ src/main/java/de/cismet/cismap/commons/HeadlessMapProvider.java
@@ -20,7 +20,6 @@ import java.beans.PropertyChangeListener;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
