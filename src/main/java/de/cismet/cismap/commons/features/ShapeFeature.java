4512dbcf20150834da02497a6fdc0aad47b37567
therter <thorsten.herter@cismet.de>
2014-02-12 14:28:04 +0100

    #118 consider geometries with measurement values

--- src/main/java/de/cismet/cismap/commons/features/ShapeFeature.java
+++ src/main/java/de/cismet/cismap/commons/features/ShapeFeature.java
@@ -12,104 +12,145 @@
 package de.cismet.cismap.commons.features;
 
 import com.vividsolutions.jts.geom.Geometry;
+
+import org.deegree.model.feature.FeatureProperty;
+import org.deegree.model.spatialschema.JTSAdapter;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.deegree.model.feature.FeatureProperty;
-import org.deegree.model.spatialschema.JTSAdapter;
-
 /**
- * Features read from a SHP File. 
+ * Features read from a SHP File.
  *
  * @author   Pascal Dihé
  * @version  $Revision$, $Date$
  */
 public class ShapeFeature extends DefaultFeatureServiceFeature {
 
-    //~ Instance fields --------------------------------------------------------
-    private final ShapeInfo shapeInfo;
+    //~ Static fields/initializers ---------------------------------------------
 
+    // caches the last feature properties
+    private static int lastFeatureId = -1;
+    private static LinkedHashMap<String, Object> lastFeatureContainer = null;
+
+    //~ Instance fields --------------------------------------------------------
+
+    private final ShapeInfo shapeInfo;
 
     //~ Constructors -----------------------------------------------------------
 
     /**
      * Creates a new ShapeFeature object.
      *
-     * @param  typename  DOCUMENT ME!
+     * @param  shapeInfo  typename DOCUMENT ME!
      */
 
     public ShapeFeature(final ShapeInfo shapeInfo) {
         this.shapeInfo = shapeInfo;
     }
 
-//    /**
-//     * Creates a new ShapeFeature object.
-//     *
-//     * @param  typename  DOCUMENT ME!
-//     * @param  styles    DOCUMENT ME!
-//     */
-//    public ShapeFeature(final ShapeInfo shapeInfo, final List<org.deegree.style.se.unevaluated.Style> styles) {
-//        setSLDStyles(styles); // super.style = styles;
-//        this.shapeInfo = shapeInfo;
-//    }
-
     //~ Methods ----------------------------------------------------------------
+
+// /**
+// * Creates a new ShapeFeature object.
+// *
+// * @param  typename  DOCUMENT ME!
+// * @param  styles    DOCUMENT ME!
+// */
+// public ShapeFeature(final ShapeInfo shapeInfo, final List<org.deegree.style.se.unevaluated.Style> styles) {
+// setSLDStyles(styles); // super.style = styles;
+// this.shapeInfo = shapeInfo;
+// }
 
     @Override
     public HashMap getProperties() {
-        LinkedHashMap<String, Object> container = new LinkedHashMap<String, Object>();
-            
-        try {
-            org.deegree.model.feature.Feature degreeFeature = shapeInfo.getFile().getFeatureByRecNo(getId());
-            final FeatureProperty[] featureProperties = degreeFeature.getProperties();
+        final LinkedHashMap<String, Object> container = new LinkedHashMap<String, Object>();
 
-            for (final FeatureProperty fp : featureProperties) {
-                container.put(fp.getName().getAsString(), fp.getValue());
+        if (shapeInfo.getFc() == null) {
+            try {
+                if ((lastFeatureId == getId()) && (lastFeatureContainer != null)) {
+                    return lastFeatureContainer;
+                }
+
+                final org.deegree.model.feature.Feature degreeFeature = shapeInfo.getFile().getFeatureByRecNo(getId());
+                final FeatureProperty[] featureProperties = degreeFeature.getProperties();
+
+                for (final FeatureProperty fp : featureProperties) {
+                    container.put(fp.getName().getAsString(), fp.getValue());
+                }
+
+                lastFeatureId = getId();
+                lastFeatureContainer = container;
+            } catch (final Exception e) {
+                logger.error("Cannot read properties from file.", e);
             }
-        } catch (final Exception e) {
-            logger.error("Cannot read geometry from shape file.", e);
+        } else {
+            try {
+                final org.deegree.model.feature.Feature degreeFeature = shapeInfo.getFc().getFeature(getId());
+                final FeatureProperty[] featureProperties = degreeFeature.getProperties();
+
+                for (final FeatureProperty fp : featureProperties) {
+                    container.put(fp.getName().getAsString(), fp.getValue());
+                }
+            } catch (final Exception e) {
+                logger.error("Cannot read properties from file.", e);
+            }
         }
-        
+
         return container;
     }
 
     @Override
-    public Object getProperty(String propertyName) {
+    public Object getProperty(final String propertyName) {
         return getProperties().get(propertyName);
     }
 
     @Override
-    public void setProperties(HashMap properties) {
-        //nothing to do
+    public void setProperties(final HashMap properties) {
+        // nothing to do
     }
 
     @Override
-    public void addProperty(String propertyName, Object property) {
-        //nothing to do
+    public void addProperty(final String propertyName, final Object property) {
+        // nothing to do
     }
 
-    public void addProperties(Map<String, Object> map) {
-        //nothing to do
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  map  DOCUMENT ME!
+     */
+    public void addProperties(final Map<String, Object> map) {
+        // nothing to do
     }
-    
+
     @Override
     public Geometry getGeometry() {
         Geometry g = null;
-        
-        try {
-            g = JTSAdapter.export(shapeInfo.getFile().getGeometryByRecNo(getId()));
-            g.setSRID(shapeInfo.getSrid());
-        } catch (final Exception e) {
-            logger.error("Cannot read geometry from shape file.", e);
+
+        if (shapeInfo.getFc() == null) {
+            try {
+                g = JTSAdapter.export(shapeInfo.getFile().getGeometryByRecNo(getId()));
+                g.setSRID(shapeInfo.getSrid());
+            } catch (final Exception e) {
+                logger.error("Cannot read geometry from shape file.", e);
+            }
+        } else {
+            try {
+                g = JTSAdapter.export(shapeInfo.getFc().getFeature(getId()).getDefaultGeometryPropertyValue());
+                g.setSRID(shapeInfo.getSrid());
+            } catch (final Exception e) {
+                logger.error("Cannot read geometry from shape file.", e);
+            }
         }
-        
+
         return g;
     }
 
     @Override
-    public void setGeometry(Geometry geom) {
-        //do nothing
+    public void setGeometry(final Geometry geom) {
+        // do nothing
     }
 }
--- src/main/java/de/cismet/cismap/commons/featureservice/factory/ShapeFeatureFactory.java
+++ src/main/java/de/cismet/cismap/commons/featureservice/factory/ShapeFeatureFactory.java
@@ -18,12 +18,12 @@ import org.deegree.io.rtree.HyperPoint;
 import org.deegree.io.rtree.RTree;
 import org.deegree.io.rtree.RTreeException;
 import org.deegree.io.shpapi.ShapeFile;
+import org.deegree.io.shpapi.shape_new.ShapeFileReader;
 import org.deegree.model.feature.Feature;
+import org.deegree.model.feature.FeatureCollection;
 import org.deegree.model.feature.schema.FeatureType;
 import org.deegree.model.feature.schema.PropertyType;
 import org.deegree.model.spatialschema.JTSAdapter;
-
-import org.openide.util.Exceptions;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -75,6 +75,7 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
     private int shapeSrid = 0;
     private Crs crs = CismapBroker.getInstance().getSrs();
     private org.deegree.model.spatialschema.Envelope envelope;
+    private FeatureCollection fc = null;
 
     //~ Constructors -----------------------------------------------------------
 
@@ -131,12 +132,24 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
 
     @Override
     protected ShapeFeature createFeatureInstance(final Feature degreeFeature, final int index) throws Exception {
-        //dummy method
+        // dummy method
         return null;
     }
-    
-    
-    protected ShapeFeature createFeatureInstance(final Feature degreeFeature, final ShapeInfo shapeInfo, final int index) throws Exception {
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   degreeFeature  DOCUMENT ME!
+     * @param   shapeInfo      DOCUMENT ME!
+     * @param   index          DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    protected ShapeFeature createFeatureInstance(final Feature degreeFeature,
+            final ShapeInfo shapeInfo,
+            final int index) throws Exception {
         final String filename = new File(documentURI).getName();
 //        layerName = filename;
         final ShapeFeature shapeFeature;
@@ -151,15 +164,18 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
     }
 
     @Override
-    protected void initialiseFeature(ShapeFeature featureServiceFeature, Feature degreeFeature, boolean evaluateExpressions, int index) throws Exception {
+    protected void initialiseFeature(final ShapeFeature featureServiceFeature,
+            final Feature degreeFeature,
+            final boolean evaluateExpressions,
+            final int index) throws Exception {
         // perform standard initialisation
         featureServiceFeature.setLayerProperties(this.getLayerProperties());
 
         if (evaluateExpressions) {
             this.evaluateExpressions(featureServiceFeature, index);
         }
-    }    
-    
+    }
+
     /**
      * DOCUMENT ME!
      */
@@ -251,8 +267,8 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
         envelope = shapeFile.getFileMBR();
         // create an index file, if it does not alreay exists
         int currentProgress = 0;
-        int newProgress = 0;
-        String filename = null;
+        int newProgress;
+        String filename;
 
         if (this.documentURI.getPath().endsWith(".shp")) {
             filename = this.documentURI.getPath().substring(0, this.documentURI.getPath().length() - 4);
@@ -260,12 +276,29 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
             filename = this.documentURI.getPath();
         }
 
+        final ShapeFileReader reader = new ShapeFileReader(filename);
+        final int shapeType = reader.getShapeType();
+
+        if ((shapeType == org.deegree.io.shpapi.shape_new.ShapeFile.MULTIPOINTM)
+                    || (shapeType == org.deegree.io.shpapi.shape_new.ShapeFile.POLYLINEM)
+                    || (shapeType == org.deegree.io.shpapi.shape_new.ShapeFile.POLYGONM)
+                    || (shapeType == org.deegree.io.shpapi.shape_new.ShapeFile.POINTM)) {
+            final org.deegree.io.shpapi.shape_new.ShapeFile shapeFromReader = reader.read();
+            fc = shapeFromReader.getFeatureCollection();
+        }
+
         try {
             if (!shapeFile.hasRTreeIndex()) {
                 final int features = shapeFile.getRecordNum();
                 final RTree rtree = new RTree(2, 11, filename + ".rti");
                 for (int i = 1; i < (features + 1); i++) {
-                    final Feature feature = shapeFile.getFeatureByRecNo(i);
+                    Feature feature = null;
+
+                    if (fc != null) {
+                        feature = fc.getFeature(i);
+                    } else {
+                        feature = shapeFile.getFeatureByRecNo(i);
+                    }
 
                     final org.deegree.model.spatialschema.Geometry[] geometries = feature.getGeometryPropertyValues();
 
@@ -391,7 +424,7 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
     public synchronized List<ShapeFeature> createFeatures(final String query,
             final BoundingBox boundingBox,
             final SwingWorker workerThread) throws TooManyFeaturesException, Exception {
-        return createFeatures(query, boundingBox, workerThread, 0, 0, null);
+        return createFeatures(query, boundingBox, workerThread, 0, 0, null, true);
     }
 
     @Override
@@ -526,6 +559,32 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
             final int offset,
             final int limit,
             final FeatureServiceAttribute[] orderBy) throws TooManyFeaturesException, Exception {
+        return createFeatures(query, boundingBox, workerThread, offset, limit, orderBy, false);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   query         DOCUMENT ME!
+     * @param   boundingBox   DOCUMENT ME!
+     * @param   workerThread  DOCUMENT ME!
+     * @param   offset        DOCUMENT ME!
+     * @param   limit         DOCUMENT ME!
+     * @param   orderBy       DOCUMENT ME!
+     * @param   shapeLimit    DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  TooManyFeaturesException  DOCUMENT ME!
+     * @throws  Exception                 DOCUMENT ME!
+     */
+    public synchronized List<ShapeFeature> createFeatures(final String query,
+            final BoundingBox boundingBox,
+            final SwingWorker workerThread,
+            final int offset,
+            final int limit,
+            final FeatureServiceAttribute[] orderBy,
+            final boolean shapeLimit) throws TooManyFeaturesException, Exception {
         try {
             if (!this.initialised) {
                 logger.warn("SW[" + workerThread + "]: Factory not correclty initialised, parsing shape file");
@@ -554,8 +613,7 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
             Polygon boundingPolygon = geomFactory.createPolygon(geomFactory.createLinearRing(polyCords), null);
 
             boundingPolygon = (Polygon)CrsTransformer.transformToGivenCrs(boundingPolygon, shapeCrs.getCode());
-            
-            final Charset cs = getCharsetDefinition();
+
             String filename = null;
 
             if (this.documentURI.getPath().endsWith(".shp")) {
@@ -564,15 +622,16 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
                 filename = this.documentURI.getPath();
             }
 
-            ShapeFile persShapeFile = getShapeFile();;            
-            
+            final ShapeFile persShapeFile = getShapeFile();
+
             shapeFile = null;
-            final int[] recordNumbers = shapeFile.getGeoNumbersByRect(JTSAdapter.wrap(boundingPolygon).getEnvelope());
+            final int[] recordNumbers = persShapeFile.getGeoNumbersByRect(JTSAdapter.wrap(boundingPolygon)
+                            .getEnvelope());
 
             List<ShapeFeature> selectedFeatures = new ArrayList<ShapeFeature>(recordNumbers.length);
-            ShapeInfo info = new ShapeInfo(filename, persShapeFile, shapeSrid);
+            final ShapeInfo info = new ShapeInfo(filename, persShapeFile, shapeSrid, fc);
 
-            if (recordNumbers.length < 15000) {
+            if (!shapeLimit || (recordNumbers.length < 35000)) {
                 for (final int record : recordNumbers) {
                     final Feature f = persShapeFile.getFeatureByRecNo(record);
                     final ShapeFeature featureServiceFeature = createFeatureInstance(f, info, record);
@@ -593,7 +652,7 @@ public class ShapeFeatureFactory extends DegreeFeatureFactory<ShapeFeature, Stri
             // check if thread is canceled .........................................
 
             logger.info("SW[" + workerThread + "]: " + selectedFeatures.size()
-                        + " features selected by bounding box out of " + this.shapeFile.getRecordNum()
+                        + " features selected by bounding box out of " + persShapeFile.getRecordNum()
                         + " in spatial index");
             if (logger.isDebugEnabled()) {
                 logger.debug("SW[" + workerThread + "]: quering spatial index for bounding box took "
