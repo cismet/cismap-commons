529091d9ea738b15c3b7b81d1a42c59ad0659661
JeanAtCismet <jean.ruiz@cismet.de>
2016-12-12 17:24:11 +0100

    issue #364 drophandler implementation

---
+++ src/main/java/de/cismet/cismap/commons/drophandler/builtin/MappingComponentGeoReferencedImageFileDropHandler.java
@@ -0,0 +1,82 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cismap.commons.drophandler.builtin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.io.File;
+
+import java.util.Collection;
+
+import de.cismet.cismap.commons.drophandler.MappingComponentDropHandler;
+import de.cismet.cismap.commons.drophandler.MappingComponentDropHandlerFileMatcher;
+import de.cismet.cismap.commons.gui.layerwidget.LayerDropUtils;
+import de.cismet.cismap.commons.gui.layerwidget.LayerWidget;
+import de.cismet.cismap.commons.gui.layerwidget.LayerWidgetProvider;
+import de.cismet.cismap.commons.rasterservice.ImageFileUtils;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = MappingComponentDropHandler.class)
+public class MappingComponentGeoReferencedImageFileDropHandler implements MappingComponentDropHandler,
+    LayerWidgetProvider {
+
+    //~ Instance fields --------------------------------------------------------
+
+    @Getter private final MappingComponentDropHandlerFileMatcher fileMatcher = new ImageFileMatcher();
+
+    @Getter @Setter private LayerWidget layerWidget;
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public int getPriority() {
+        return MappingComponentDropHandlerBuiltinPriorityConstants.IMAGE;
+    }
+
+    @Override
+    public void dropFiles(final Collection<File> files) {
+        for (final File file : files) {
+            LayerDropUtils.handleImageFile(
+                file,
+                layerWidget.getMappingModel(),
+                -1,
+                layerWidget,
+                ImageFileUtils.Mode.GEO_REFERENCED);
+        }
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    class ImageFileMatcher implements MappingComponentDropHandlerFileMatcher {
+
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public boolean isMatching(final File file) {
+            return ImageFileUtils.isImageFileEnding(file.getName());
+        }
+    }
+}
