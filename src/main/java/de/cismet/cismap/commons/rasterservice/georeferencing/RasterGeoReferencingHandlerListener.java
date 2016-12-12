aba027c9bcd97ad62dbd7797581b95029ec88a71
JeanAtCismet <jean.ruiz@cismet.de>
2016-12-12 17:23:30 +0100

    issue #364 initial backend implementation

---
+++ src/main/java/de/cismet/cismap/commons/rasterservice/georeferencing/RasterGeoReferencingHandlerListener.java
@@ -0,0 +1,64 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cismap.commons.rasterservice.georeferencing;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import java.awt.Point;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public interface RasterGeoReferencingHandlerListener {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  position  DOCUMENT ME!
+     */
+    void positionAdded(final int position);
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  position  DOCUMENT ME!
+     */
+    void positionRemoved(final int position);
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  position  DOCUMENT ME!
+     */
+    void positionChanged(final int position);
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  position  DOCUMENT ME!
+     */
+    void pointSelected(final int position);
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  position  DOCUMENT ME!
+     */
+    void coordinateSelected(final int position);
+}
