daa9887519f3d0c97a87d0b915e9906cd070a1fe
therter <thorsten.herter@cismet.de>
2024-02-13 19:06:54 +0100

    #532 add the possibility to add a simplifier

---
+++ src/main/java/de/cismet/cismap/commons/features/DefaultFeatureSimplifier.java
@@ -0,0 +1,32 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cismap.commons.features;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class DefaultFeatureSimplifier implements FeatureSimplifier {
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Geometry simplify(final Feature feature) {
+        final Geometry geom = feature.getGeometry();
+
+        return geom;
+    }
+}
